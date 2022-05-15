57e9b5365f8f7fdd1bf78e7c3ad14cad280dadbb
Rishav_Kumar <rishavkumaraug20005212@gmail.com>
2022-05-15 19:32:17 +0530

    dimentions corrected

--- src/Colors.js
+++ src/Colors.js
@@ -3,14 +3,14 @@ const AppTheme={
         textColor:"black",
         backgroundColor:"#bec4c4",
         padding:"5px",
-        margin:"5px",
+        margin:"15px",
         border:"2px solid black"
     },
     dark:{
         textColor:"yellow",
         backgroundColor:"black",
         padding:"5px",
-        margin:"5px",
+        margin:"15px",
         border:"2px dotted white"
     }
 }
